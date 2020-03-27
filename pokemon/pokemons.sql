64249b561f6a0290ec1d6955dfd5d3874757f2a2
Manuel Sánchez <contacto@manuelsanchez.com.mx>
2020-03-26 17:42:16 -0700

    Update pokemons.sql
    Se agrega el currrent_timesamp en el  CREATE y en el update

--- pokemon/pokemons.sql
+++ pokemon/pokemons.sql
@@ -1,8 +1,10 @@
+DROP TABLE if EXISTS pokemons;
+
 CREATE TABLE `pokemons` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  user_id int NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
